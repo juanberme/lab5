e664175d19188603b616c764bbbae0981b3d106c
juan camilo bermeo <camilo22601@gmail.com>
2020-04-01 16:30:54 -0500

    primero

---
+++ lab5/src/lab5/Lab5.java
@@ -0,0 +1,12 @@
+package lab5;
+
+import processing.core.PApplet;
+
+public class Lab5 extends PApplet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main("lab5.Lab5");
+	}
+
+}
